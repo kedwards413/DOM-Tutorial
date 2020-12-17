2d234cd16eb637a81b679e407bae4d2fa0e88bfc
kedwards413 <kaela.rose.edwards@gmail.com>
2020-12-17 01:09:45 +0000

    initial commit

--- .breathecode/resets/13-Add-Options-to-the-Select/index.js
+++ .breathecode/resets/13-Add-Options-to-the-Select/index.js
@@ -2,8 +2,5 @@ window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
-	var mySelect = document.querySelector("#mySelect");
-	for (let country of countries) {
-		mySelect.innerHTML += `<option value=''> ${country} </option>`;
-	}
+  // your code here
 };
--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,14 @@
-// Your code here
+let taskList = document.querySelector("#addToDo");
+taskList.addEventListener("keyup", addToDo);
+
+let ul = document.querySelector("ul");
+
+function addToDo(event) {
+	if (event.keyCode == 13) {
+		let task = event.target.value;
+		ul.innerHTML += `<li>
+					<span><i class="fa fa-trash"></i></span> ${task}
+				</li>`;
+		console.log(event);
+	}
+}

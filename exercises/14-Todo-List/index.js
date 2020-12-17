fe7419755f4226db7304230490a83dece8dbcb01
kedwards413 <kaela.rose.edwards@gmail.com>
2020-12-17 02:03:22 +0000

    updates

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,14 +1,23 @@
-let taskList = document.querySelector("#addToDo");
-taskList.addEventListener("keyup", addToDo);
+let input = document.querySelector("#addToDo");
+input.addEventListener("keyup", addTodo);
 
 let ul = document.querySelector("ul");
 
-function addToDo(event) {
+function deleteTodo(event) {
+	var tasks = document.querySelectorAll(".fa-trash");
+	tasks.forEach((item, index) => {
+		item.addEventListener("click", function(event) {
+			let deletedItem = event.target.parentNode.parentNode;
+			deletedItem.parentNode.removeChild(deletedItem);
+		});
+	});
+}
+function addTodo(event) {
 	if (event.keyCode == 13) {
 		let task = event.target.value;
-		ul.innerHTML += `<li>
-					<span><i class="fa fa-trash"></i></span> ${task}
-				</li>`;
+		ul.innerHTML += `<li><span><i class="fa fa-trash"></i></span> ${task}</li>`;
+		deleteTodo();
 		console.log(event);
 	}
 }
+deleteTodo();
